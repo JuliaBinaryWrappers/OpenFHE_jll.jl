726725696427aa55d1347c0b174db4a6003fe2be
jlbuild <juliabuildbot@gmail.com>
2026-04-13 16:37:11 +0000

    OpenFHE_jll build 1.5.0+0

--- src/OpenFHE_jll.jl
+++ src/OpenFHE_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("OpenFHE")
-JLLWrappers.@generate_main_file("OpenFHE", UUID("a2687184-f17b-54bc-b2bb-b849352af807"))
+JLLWrappers.@generate_main_file("OpenFHE", Base.UUID("a2687184-f17b-54bc-b2bb-b849352af807"))
 end  # module OpenFHE_jll
